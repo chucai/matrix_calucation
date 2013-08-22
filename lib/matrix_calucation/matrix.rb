16780abc67bcbe132e0fc5d802ec198ba01af161
xdhe <hexudong08@gmail.com>
2013-08-22 21:02:28 +0800

    implement basic auth

--- lib/matrix_calucation/matrix.rb
+++ lib/matrix_calucation/matrix.rb
@@ -1,7 +1,8 @@
 module MatrixCalucation
 	class Matrix
 		def initialize(*args)
-			@items = args
+			@items = args unless args.empty?
+			@items ||= []
 		end
 
 		def sum(n = nil)
@@ -12,6 +13,44 @@ module MatrixCalucation
 				6
 			end
 		end
+
+		def convert_to_array
+			@array = []
+			(0...one_convert_number).each do |i|
+				case i
+				when 0...x
+					@array << @items.first[i]
+				when x...(x+y-2)
+					@array << @items[i+1-x].last
+				when ((x+y-2)..(2*x+y-3))
+					last = @items.last
+					index = 2*x+y-i-3
+					@array << last[index]
+				when ((2*x+y-2)...one_convert_number)
+					index = one_convert_number - (2*x + y - 2)
+					@array << @items[index].first
+				end
+			end
+			@array
+		end
+
+		#每循环一次需要处理的数据量
+		def one_convert_number
+			2*(x+y) - 4
+		end
+
+		def x
+			@items.first.size 
+		end
+
+		def y 
+			@items.size
+		end
+
+		def append(array)
+			@items << array
+		end
+
 
 		def size
 			@size ||= @items.flatten.size
--- spec/matrix_calucation_spec.rb
+++ spec/matrix_calucation_spec.rb
@@ -9,6 +9,9 @@ describe  MatrixCalucation do
 		MatrixCalucation::VERSION.should eql '0.0.1'
 	end
 
+	#
+	#  1 2
+	#  3 4
 	context "simple" do 
 		before do 
 			@matrix = MatrixCalucation::Matrix.new([1,2], [3,4])
@@ -30,6 +33,41 @@ describe  MatrixCalucation do
 			end
 		end
 
+		describe "convert_to_array" do
+			it "should eql [1,2,3,4]" do 
+				@matrix.convert_to_array.should eql [1,2,4,3]
+			end 
+		end
+
+		describe "one_convert_number" do 
+			it "should eql 4" do 
+				@matrix.one_convert_number.should eql 4
+			end
+		end
+	end
+
+	##
+	##
+	# 12 32 09 11 34
+	#	08 54 76 23 07
+  #	27 18 25 09 43
+	#	11 23 78 63 19
+	#	09 22 56 31 05
+	context "complex" do 
+		before do 
+			@matrix =  MatrixCalucation::Matrix.new
+			@matrix.append(%w(12 32 09 11 34))
+			@matrix.append(%w(08 54 76 23 07))
+			@matrix.append(%w(27 18 25 09 43))
+			@matrix.append(%w(11 23 78 63 19))
+			@matrix.append(%w(09 22 56 31 05))
+		end
+
+		describe "one_convert_number" do 
+			it "should eql 16" do 
+				@matrix.one_convert_number.should eql 16
+			end
+		end
 	end
 
 end
